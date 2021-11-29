848a708c812c36ec2171a03b729a32cb888fd007
AlexandreHnf <aheneffe@ulb.ac.be>
2021-11-29 14:29:11 +0100

    replaced incremental ids to installations IDs + sorted

--- XLStoCsv.py
+++ XLStoCsv.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 
 xls_file_path = "sensors/FluksoTechnical.xlsx"
-csv_file_path = "sensors/FluksoTechnical.csv"
+csv_file_path = "sensors/FluksoTechnical_Flukso.csv"
 
-read_file = pd.read_excel(xls_file_path, sheet_name="Sensors")
+read_file = pd.read_excel(xls_file_path, sheet_name="Flukso")
+print(read_file.head(10))
 read_file.to_csv(csv_file_path, index=None, header=True)
--- arrangeFluksoSensors.py
+++ arrangeFluksoSensors.py
@@ -3,22 +3,62 @@ import numpy as np
 
 START_ID = 4
 SENSOR_FILE = "sensors/FluksoTechnical.csv"
+SENSOR_FLUKSO_FILE = "sensors/FluksoTechnical_Flukso.csv"
 COMPACT_SENSOR_FILE = "sensors/sensors_technical.csv"
 
 
-def getHomeIDs(flukso_ids):
-    home_ids = []
-    current_id = None
-    new_id = START_ID - 1
+def getHomeIDcolumn(flukso_ids, home_ids):
+    home_ids_col = []
     for fi in flukso_ids:
-        if current_id is None or fi != current_id:
-            new_id += 1
-            current_id = fi
+        home_ids_col.append(home_ids[fi])
 
-        home_ids.append(new_id)
+    print(home_ids_col)
+    return home_ids_col
 
-    print(home_ids)
-    return home_ids
+
+def getInstallationsIds(flukso_ids):
+    installation_ids_df = pd.read_csv(SENSOR_FLUKSO_FILE)
+    fluksos = {}
+
+    for i in range(len(installation_ids_df)):
+        FlmId = installation_ids_df["FlmId"][i]
+        installation_id = installation_ids_df["InstallationId"][i]
+        FluksoId = installation_ids_df["FluksoId"][i]
+        print(FlmId, installation_id, FluksoId)
+
+        if str(FlmId) == "nan":
+            FlmId = FluksoId
+
+        fluksos[FlmId] = installation_id
+
+    installation_id_col = []
+    for fi in flukso_ids:
+        installation_id_col.append(fluksos[fi])
+    return installation_id_col
+
+
+# def getHomeIDs():
+#     installation_ids_df = pd.read_csv(SENSOR_FLUKSO_FILE)
+#     home_ids = {}
+#     installation_indexes = {}
+#     index = 0
+#     for i in range(len(installation_ids_df)):
+#         FlmId = installation_ids_df["FlmId"][i]
+#         installation_id = installation_ids_df["InstallationId"][i]
+#         FluksoId = installation_ids_df["FluksoId"][i]
+#         print(FlmId, installation_id, FluksoId)
+#
+#         if str(FlmId) == "nan":
+#             FlmId = FluksoId
+#
+#         if installation_id not in installation_indexes:
+#             index += 1
+#             installation_indexes[installation_id] = index
+#
+#         home_ids[FlmId] = index
+#
+#     print("installation_indexes : ", installation_indexes)
+#     return home_ids
 
 
 def getStateFromPhase(phase_names):
@@ -36,21 +76,26 @@ def getCompactSensorDF():
     sensors_df = pd.read_csv(SENSOR_FILE, header=0, index_col=1)
     compact_df = pd.DataFrame(columns=["home_ID",
                                        "phase",
-                                       "home_name",
+                                       "flukso_id",
                                        "sensor_id",
                                        "token",
                                        "state"])
 
     compact_df["phase"] = sensors_df["Function"]
-    compact_df["home_name"] = sensors_df["FlmId"]
+    compact_df["flukso_id"] = sensors_df["FlmId"]
     compact_df["sensor_id"] = sensors_df["SensorId"]
     compact_df["token"] = sensors_df["Token"]
 
-    home_ids = getHomeIDs(sensors_df["FlmId"])
-    compact_df["home_ID"] = home_ids
+    # home_ids = getHomeIDs()
+    installation_ids_col = getInstallationsIds(sensors_df["FlmId"])
+    compact_df["home_ID"] = installation_ids_col
+    # home_ids_col = getHomeIDcolumn(sensors_df["FlmId"], home_ids)
+    # compact_df["home_ID"] = home_ids_col
 
     states = getStateFromPhase(compact_df["phase"])
     compact_df["state"] = states
+
+    compact_df.sort_values(by=["home_ID"])
 
     print(compact_df.head(10))
 
