7091f1f0ecdefbaf33915749204d140028b2a60a
AlexandreHnf <aheneffe@ulb.ac.be>
2021-12-02 17:24:13 +0100

    get file size estimate for 1 hour, 1 day, 1 month and 1 year with 17 homes, 68 fluksos sensors

--- estimateDatabaseSize.py
+++ estimateDatabaseSize.py
@@ -1,17 +1,26 @@
 import fluksoVisualizationWithGUI as fv
 import os
+import json
 
 
 def showSize(fsize):
     b = fsize
     kb = fsize * 0.001
     mb = fsize * 0.000001
-    print("- {} Bytes = {} Kb = {} Mb".format(b, kb, mb))
+    gb = fsize * 0.000000001
+    print("- {} Bytes = {} Kb = {} Mb = {} Gb".format(b, kb, mb, gb))
+    return {"b": b, "kb": kb, "mb": mb, "gb": gb}
 
 
 def size(filename):
     fsize = os.path.getsize(filename)
     return fsize
+
+
+def toJson(sizes, filename):
+    print(filename)
+    with open(filename, 'w') as f:
+        json.dump(sizes, f)
 
 
 def estimate(since):
@@ -25,17 +34,26 @@ def estimate(since):
     total_size = 0
     nb_tot_sensors = 0
 
+    sizes = {}
+
     for hid, home in homes.items():
-        print("==================================================================")
         nb_sensors = home.getNbFluksoSensors()
         print("Home {}, {} fluksos sensors : ".format(hid, nb_sensors))
         nb_tot_sensors += nb_sensors
         fsize = size("output/fluksoData/{}.csv".format(hid))
-        showSize(fsize)
+        size_dic = showSize(fsize)
         total_size += fsize
+        sizes[hid] = size_dic
 
     print("TOTAL SIZE ({} homes, {} sensors) : ".format(len(homes), nb_tot_sensors))
-    showSize(total_size)
+    tot_size_dic = showSize(total_size)
+    sizes["Total"] = tot_size_dic
+
+    print(sizes)
+
+    toJson(sizes, "output/fluksoData/sizes_{}.json".format(since))
+
+    return sizes
 
 
 def main():
@@ -43,12 +61,14 @@ def main():
     size(filename)
 
     # 1 flukso(fonctionnel):
-    # - 20 min :
-    estimate("1hours")
+    # - 1 hour:
+    sizes_1h = estimate("1hours")
+    # - 1 day :
+    sizes_1d = estimate("1days")
     # - 1 month:
-    # estimate("30days")
+    sizes_1m = estimate("30days")
     # - 1 year:
-    # estimate("365days")
+    sizes_1y = estimate("365days")
     # - all:
     # estimate("")
 
