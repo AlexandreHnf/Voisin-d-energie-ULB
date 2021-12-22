658fb23558c92456b5a01c72d90e80ccb6cc16b2
AlexandreHnf <Alexandre.Heneffe@ulb.be>
2021-12-22 11:46:28 +0100

    successfully inserted test data to cassandra using python driver

--- webapp/pyToCassandra.py
+++ webapp/pyToCassandra.py
@@ -1,10 +1,22 @@
-from cassandra.cluster import cluster
+from cassandra.cluster import Cluster
 
-cluster = cluster()
+# create the cluster : connects to localhost (127.0.0.1:9042) by default
+cluster = Cluster()
 
+# connect to the keyspace or create one if it doesn't exist
 session = cluster.connect('test_keyspace')
 
-session.execute("INSERT INTO table_name (id ...) VALUES (uuid(), ...)")
+# to create a new keyspace : 
+# keyspace_query = "CREATE KEYSPACE test_keyspace WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 1};"
+# session.execute(keyspace_query)
+
+# create a new table : 
+session.execute("CREATE TABLE IF NOT EXISTS test_keyspace.fruit_stock \
+                (item_id TEXT, name TEXT, price_p_item DECIMAL, PRIMARY KEY (item_id));")
+
+# insert new row
+session.execute("INSERT INTO test_keyspace.fruit_stock (item_id, name, price_p_item) \
+                VALUES ('a0','apples',0.50);")
 
 print("Insertion done.")
 
