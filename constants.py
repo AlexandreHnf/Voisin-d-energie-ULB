8b51c59a36b5db7149f53def809357d4236a39b9
AlexandreHnf <Alexandre.Heneffe@ulb.be>
2021-12-24 12:06:18 +0100

    constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+""" 
+Constants
+"""
+
+# files paths
+SENSOR_FILE = "sensors/sensors.csv"
+UPDATED_SENSORS_FILE = "sensors/updated_sensors.csv"
+
+OUTPUT_FILE = 'output/fluksoData/'
+
+GROUPS_FILE = "sensors/grouped_homes_sensors.txt"
+
+
+# sample frequency
+FREQ = [8, "S"]  # 8 sec.
