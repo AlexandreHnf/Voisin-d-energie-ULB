fd4fcf3f66fd8542332cae533893afc9f3ae8764
AlexandreHnf <aheneffe@ulb.ac.be>
2022-01-14 16:01:08 +0100

    fix problem with timestamps locality for raw data and stats

--- fluksoStats.py
+++ fluksoStats.py
@@ -10,131 +10,134 @@ import pandas as pd
 
 
 def read_sensor_info(sensor_file):
-    """
-    read csv file of sensors data
-    """
-    sensor_file
-    sensors = pd.read_csv(sensor_file, header=0, index_col=1)
-    return sensors
+	"""
+	read csv file of sensors data
+	"""
+	sensor_file
+	sensors = pd.read_csv(sensor_file, header=0, index_col=1)
+	return sensors
 
 
 def getTiming(t, now):
-    """
-    get the timestamp of the "since"
-    ex : the timestamp 20 min ago
-    """
-    print("since {}".format(t))
-    timing = 0
-    if t:
-        if t[0] == "s":
-            e = t[1:].split("-")
-            timing = pd.Timestamp(year=int(e[0]), month=int(e[1]), day=int(e[2]),
-                                  hour=int(e[3]), minute=int(e[4]), tz="CET")
-        else:
-            print("time delta : ", pd.Timedelta(t))
-            print("now : ", now)
-            timing = now - pd.Timedelta(t)
+	"""
+	get the timestamp of the "since"
+	ex : the timestamp 20 min ago
+	"""
+	print("since {}".format(t))
+	timing = 0
+	if t:
+		if t[0] == "s":
+			e = t[1:].split("-")
+			timing = pd.Timestamp(year=int(e[0]), month=int(e[1]), day=int(e[2]),
+								  hour=int(e[3]), minute=int(e[4]), tz="CET")
+		else:
+			print("time delta : ", pd.Timedelta(t))
+			print("now : ", now)
+			timing = now - pd.Timedelta(t)
 
-    print("timing : ", timing)
-    return timing
+	print("timing : ", timing)
+
+	return timing
 
 
 def getRawData(session, since, home_ids):
-    """ 
-    get raw flukso data from cassandra since a certain amount of time
-    """
-    now = pd.Timestamp.now(tz="UTC")
-    timing = getTiming(since, now)
+	""" 
+	get raw flukso data from cassandra since a certain amount of time
+	"""
+	now = pd.Timestamp.now(tz="CET")
+	timing = getTiming(since, now)
 
-    print("timing date : ", str(timing.date()))
-    print("now date : ", str(now.date()))
-    dates = list(set(["'"+str(timing.date())+"'", "'"+str(now.date())+"'"]))
-    print(dates)
-    dates = ",".join(dates)
+	print("timing date : ", str(timing.date()))
+	print("now date : ", str(now.date()))
+	dates = list(set(["'"+str(timing.date())+"'", "'"+str(now.date())+"'"]))
+	print(dates)
+	dates = ",".join(dates)
+	timing_format = "'" + str(timing)[:19] + ".000000+0000" + "'"
 
-    homes_rawdata = {}
-    for home_id in home_ids:
-        
-        where_clause = "home_id = {} and day IN ({}) AND ts > {}".format("'"+home_id+"'", dates, "'"+str(timing)+"'")
-        data = ptc.selectQuery(session, CASSANDRA_KEYSPACE, "raw_data", "*", where_clause, "LIMIT 5")
+	homes_rawdata = {}
+	for home_id in home_ids:
+		
+		where_clause = "home_id = {} and day IN ({}) AND ts > {}".format("'"+home_id+"'", dates, timing_format)
+		data = ptc.selectQuery(session, CASSANDRA_KEYSPACE, "raw_data", "*", where_clause, "LIMIT 10")
 
-        # print(home_id)
-        # print(data)
+		# print(home_id)
+		# print(data)
 
-        homes_rawdata[home_id] = data
+		homes_rawdata[home_id] = data
 
-    return homes_rawdata
-        
+	return homes_rawdata
+		
 
 def getConsumptionProductionDF(sensors, homes_rawdata, home_ids):
-        """ 
-        P_cons = P_tot - P_prod
-        P_net = P_prod + P_cons
-        """
-        homes_stats = {}
-        for home_id in home_ids:
-            power_df = homes_rawdata[home_id]
-            home_sensors = sensors.loc[sensors["home_ID"] == home_id]
-            cons_prod_df = power_df[["home_id","day", "ts"]].copy()
-            cons_prod_df["P_cons"] = 0
-            cons_prod_df["P_prod"] = 0
-            cons_prod_df["P_tot"] = 0
+		""" 
+		P_cons = P_tot - P_prod
+		P_net = P_prod + P_cons
+		"""
+		homes_stats = {}
+		for home_id in home_ids:
+			power_df = homes_rawdata[home_id]
+			home_sensors = sensors.loc[sensors["home_ID"] == home_id]
+			cons_prod_df = power_df[["home_id","day", "ts"]].copy()
+			cons_prod_df["P_cons"] = 0
+			cons_prod_df["P_prod"] = 0
+			cons_prod_df["P_tot"] = 0
 
-            for i, phase in enumerate(home_sensors.index):
-                phase_i = "phase" + str(i+1)
-                p = home_sensors.loc[phase]["pro"]
-                n = home_sensors.loc[phase]["net"]
+			for i, phase in enumerate(home_sensors.index):
+				phase_i = "phase" + str(i+1)
+				p = home_sensors.loc[phase]["pro"]
+				n = home_sensors.loc[phase]["net"]
 
-                cons_prod_df["P_prod"] = cons_prod_df["P_prod"] + p * power_df[phase_i]
-                cons_prod_df["P_tot"] = cons_prod_df["P_tot"] + n * power_df[phase_i]
+				cons_prod_df["P_prod"] = cons_prod_df["P_prod"] + p * power_df[phase_i]
+				cons_prod_df["P_tot"] = cons_prod_df["P_tot"] + n * power_df[phase_i]
 
-            cons_prod_df["P_cons"] = cons_prod_df["P_tot"] - cons_prod_df["P_prod"]
+			cons_prod_df["P_cons"] = cons_prod_df["P_tot"] - cons_prod_df["P_prod"]
 
-            cons_prod_df = cons_prod_df.round(1)  # round all column values with 2 decimals
-            homes_stats[home_id] = cons_prod_df
+			cons_prod_df = cons_prod_df.round(1)  # round all column values with 2 decimals
+			homes_stats[home_id] = cons_prod_df
 
-            # print(home_id)
-            # print(cons_prod_df)
+			# print(home_id)
+			# print(cons_prod_df)
 
-        return homes_stats
+		return homes_stats
 
 
 def saveStatsToCassandra(session, homes_stats):
-    """ 
-    Save the stats (P_cons, P_prod, P_tot) of the raw data
-    of some period of time in Cassandra
-    """
-    print("saving in Cassandra : flukso.stats table...")
+	""" 
+	Save the stats (P_cons, P_prod, P_tot) of the raw data
+	of some period of time in Cassandra
+	"""
+	print("saving in Cassandra : flukso.stats table...")
 
-    for hid, cons_prod_df in homes_stats.items():
-        print(hid)
-        
-        col_names = list(cons_prod_df.columns)
-        for _, row in cons_prod_df.iterrows():
-            values = list(row)
-            values[2] = str(values[2])  # timestamp (ts)
-            print(values)
-            ptc.insert(session, CASSANDRA_KEYSPACE, "stats", col_names, values)
+	for hid, cons_prod_df in homes_stats.items():
+		print(hid)
+		
+		col_names = list(cons_prod_df.columns)
+		for _, row in cons_prod_df.iterrows():
+			values = list(row)
+			values[2] = str(values[2]) + "Z"  # timestamp (ts)
+			print(values)
+			ptc.insert(session, CASSANDRA_KEYSPACE, "stats", col_names, values)
 
-    print("Successfully saved stats in Cassandra")
+	print("Successfully saved stats in Cassandra")
 
 
 def main():
-    session = ptc.connectToCluster(CASSANDRA_KEYSPACE)
-    sensors = read_sensor_info(UPDATED_SENSORS_FILE)
-    home_ids = set(sensors.home_ID)
+	session = ptc.connectToCluster(CASSANDRA_KEYSPACE)
+	sensors = read_sensor_info(UPDATED_SENSORS_FILE)
+	home_ids = set(sensors.home_ID)
 
-    # test raw data retrieval
-    since = "s2022-01-13-16-26-56"
-    homes_rawdata = getRawData(session, since, home_ids) 
+	# test raw data retrieval
+	since = "s2022-01-14-15-20-00"
+	# since = "3min"
+	homes_rawdata = getRawData(session, since, home_ids) 
 
-    print("==============================================")
+	print("==============================================")
 
-    # test stats computations
-    homes_stats = getConsumptionProductionDF(sensors, homes_rawdata, home_ids)
+	# test stats computations
+	homes_stats = getConsumptionProductionDF(sensors, homes_rawdata, home_ids)
 
-    saveStatsToCassandra(session, homes_stats)
+	saveStatsToCassandra(session, homes_stats)
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- rawFluksoData.py
+++ rawFluksoData.py
@@ -36,190 +36,193 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def read_sensor_info(path, sensor_file):
-    """
-    read csv file of sensors data
-    """
-    path += sensor_file
-    sensors = pd.read_csv(path, header=0, index_col=1)
-    return sensors
+	"""
+	read csv file of sensors data
+	"""
+	path += sensor_file
+	sensors = pd.read_csv(path, header=0, index_col=1)
+	return sensors
 
 
 def getTiming(t):
-    """
-    get the timestamp of the "since"
-    ex : the timestamp 20 min ago
-    """
-    # print("since {}".format(since))
-    timing = 0
-    if t:
-        if t[0] == "s":
-            e = t[1:].split("-")
-            timing = pd.Timestamp(year=int(e[0]), month=int(e[1]), day=int(e[2]),
-                                  hour=int(e[3]), minute=int(e[4]), tz="CET").tz_convert("UTC")
-        else:
-            print("time delta : ", pd.Timedelta(t))
-            timing = pd.Timestamp.now(tz="UTC") - pd.Timedelta(t)
+	"""
+	get the timestamp of the "since"
+	ex : the timestamp 20 min ago
+	"""
+	# print("since {}".format(since))
+	timing = 0
+	if t:
+		if t[0] == "s":
+			e = t[1:].split("-")
+			timing = pd.Timestamp(year=int(e[0]), month=int(e[1]), day=int(e[2]),
+								  hour=int(e[3]), minute=int(e[4]), tz="CET").tz_convert("UTC")
+		else:
+			print("time delta : ", pd.Timedelta(t))
+			timing = pd.Timestamp.now(tz="UTC") - pd.Timedelta(t)
 
-    print("timing : ", timing)
-    return timing
+	print("timing : ", timing)
+	return timing
 
 
 def getProgDir():
-    import __main__
-    main_path = os.path.abspath(__main__.__file__)
-    main_path = os.path.dirname(main_path) + os.sep
-    return main_path
+	import __main__
+	main_path = os.path.abspath(__main__.__file__)
+	main_path = os.path.dirname(main_path) + os.sep
+	return main_path
 
 
 def generateHomes(session, sensors, since, since_timing, to_timing, home_ids):
-    homes = {}
+	homes = {}
 
-    for hid in home_ids:
-        print("========================= HOME {} =====================".format(hid))
-        home = Home(session, sensors, since, since_timing, to_timing, hid)
-        homes[hid] = home
+	for hid in home_ids:
+		print("========================= HOME {} =====================".format(hid))
+		home = Home(session, sensors, since, since_timing, to_timing, hid)
+		homes[hid] = home
+		# print(home.getPowerDF().head(10))
 
-    return homes
+	return homes
 
 
 def generateGroupedHomes(homes, groups):
-    grouped_homes = []
-    for i, group in enumerate(groups):  # group = tuple (home1, home2, ..)
-        print("========================= GROUP {} ====================".format(i + 1))
-        home = copy.copy(homes[group[0]])
-        for j in range(1, len(group)):
-            print(homes[group[j]].getHomeID())
-            home.appendFluksoData(homes[group[j]].getPowerDF(), homes[group[j]].getHomeID())
-            home.addConsProd(homes[group[j]].getConsProdDF())
-        home.setHomeID("group_" + str(i + 1))
+	grouped_homes = []
+	for i, group in enumerate(groups):  # group = tuple (home1, home2, ..)
+		print("========================= GROUP {} ====================".format(i + 1))
+		home = copy.copy(homes[group[0]])
+		for j in range(1, len(group)):
+			print(homes[group[j]].getHomeID())
+			home.appendFluksoData(homes[group[j]].getPowerDF(), homes[group[j]].getHomeID())
+			home.addConsProd(homes[group[j]].getConsProdDF())
+		home.setHomeID("group_" + str(i + 1))
 
-        grouped_homes.append(home)
+		grouped_homes.append(home)
 
-    return grouped_homes
+	return grouped_homes
 
 
 def getFluksoData(sensor_file, path=""):
-    """
-    get Flukso data (via API) 
-    """
-    if not path:
-        path = getProgDir()
+	"""
+	get Flukso data (via API) 
+	"""
+	if not path:
+		path = getProgDir()
 
-    sensors = read_sensor_info(path, sensor_file)
-    # print(sensors.head(5))
-    session = tmpo.Session(path)
-    for hid, hn, sid, tk, n, c, p in sensors.values:
-        session.add(sid, tk)
+	sensors = read_sensor_info(path, sensor_file)
+	# print(sensors.head(5))
+	session = tmpo.Session(path)
+	for hid, hn, sid, tk, n, c, p in sensors.values:
+		session.add(sid, tk)
 
-    session.sync()
+	session.sync()
 
-    return sensors, session
+	return sensors, session
 
 
 def getFLuksoGroups():
-    """
-    returns Groups with format : [[home_ID1, home_ID2], [home_ID3, home_ID4], ...]
-    """
-    groups = []
-    with open(GROUPS_FILE) as f:
-        lines = f.readlines()
-        for line in lines:
-            groups.append(line.strip().split(","))
+	"""
+	returns Groups with format : [[home_ID1, home_ID2], [home_ID3, home_ID4], ...]
+	"""
+	groups = []
+	with open(GROUPS_FILE) as f:
+		lines = f.readlines()
+		for line in lines:
+			groups.append(line.strip().split(","))
 
-    return groups
+	return groups
 
 
 # ====================================================================================
 def getColumnsNames(columns):
-    res = []
-    for i in range(len(columns)):
-        index = str(i+1)
-        # if len(index) == 1:
-        #     index = "0" + index
-        res.append("phase" + index)
+	res = []
+	for i in range(len(columns)):
+		index = str(i+1)
+		# if len(index) == 1:
+		#     index = "0" + index
+		res.append("phase" + index)
 
-    return res
+	return res
 
 
 def saveFluksoDataToCassandra(homes):
-    """
-    Save flukso data to Cassandra cluster
-    """
-    print("saving in Cassandra...")
-    session = ptc.connectToCluster(CASSANDRA_KEYSPACE)
+	"""
+	Save flukso data to Cassandra cluster
+	"""
+	print("saving in Cassandra...")
+	session = ptc.connectToCluster(CASSANDRA_KEYSPACE)
 
-    for hid, home in homes.items():
-        print(hid)
-        power_df = home.getPowerDF()
+	for hid, home in homes.items():
+		print(hid)
+		power_df = home.getPowerDF()
 
-        col_names = ["home_id", "day", "ts"] + getColumnsNames(power_df.columns)
-        # print(col_names)
-        for timestamp, row in power_df.iterrows():
-            # print(timestamp, list(row))
-            day = str(timestamp.date())
-            ts = str(timestamp)
-            values = [hid, day, ts] + list(row)
-            ptc.insert(session, CASSANDRA_KEYSPACE, "raw_data", col_names, values)
+		col_names = ["home_id", "day", "ts"] + getColumnsNames(power_df.columns)
+		# print(col_names)
+		for timestamp, row in power_df.iterrows():
+			# print(timestamp, list(row))
+			day = str(timestamp.date())
+			# save timestamp with CET local timezone, format : YY-MM-DD H:M:SZ
+			ts = str(timestamp)[:19] + "Z"  
+			print(ts)
+			values = [hid, day, ts] + list(row)
+			ptc.insert(session, CASSANDRA_KEYSPACE, "raw_data", col_names, values)
 
-    print("Successfully Saved in Cassandra")
+	print("Successfully Saved in Cassandra")
 
 
 # ====================================================================================
 
 
 def main():
-    # TODO : add argument for choosing between different features (visualizeFluksoData, identifyPhaseState)
-    argparser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-    argparser.add_argument("--since",
-                           type=str,
-                           default="",
-                           help="Period to query until now, e.g. "
-                                "'30days', "
-                                "'1hours', "
-                                "'20min',"
-                                "'s2021-12-06-16-30-00', etc. Defaults to all data.")
+	# TODO : add argument for choosing between different features (visualizeFluksoData, identifyPhaseState)
+	argparser = argparse.ArgumentParser(
+		description=__doc__,
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+	)
+	argparser.add_argument("--since",
+						   type=str,
+						   default="",
+						   help="Period to query until now, e.g. "
+								"'30days', "
+								"'1hours', "
+								"'20min',"
+								"'s2021-12-06-16-30-00', etc. Defaults to all data.")
 
-    argparser.add_argument("--to",
-                           type=str,
-                           default="",
-                           help="Query a defined interval, e.g. "
-                                "'2021-10-29-00-00-00>2021-10-29-23-59-52'")
+	argparser.add_argument("--to",
+						   type=str,
+						   default="",
+						   help="Query a defined interval, e.g. "
+								"'2021-10-29-00-00-00>2021-10-29-23-59-52'")
 
-    args = argparser.parse_args()
-    since = args.since
-    print("since : ", since)
+	args = argparser.parse_args()
+	since = args.since
+	print("since : ", since)
 
-    to = args.to
-    print("to: ", to)
+	to = args.to
+	print("to: ", to)
 
-    # =========================================================
+	# =========================================================
 
-    start_timing = getTiming(since)
-    to_timing = getTiming(to)
-    sensors, session = getFluksoData(UPDATED_SENSORS_FILE)
+	start_timing = getTiming(since)
+	to_timing = getTiming(to)
+	sensors, session = getFluksoData(UPDATED_SENSORS_FILE)
 
-    home_ids = set(sensors["home_ID"])
-    nb_homes = len(home_ids)
-    print("Number of Homes : ", nb_homes)
-    print("Number of Fluksos : ", len(sensors))
+	home_ids = set(sensors["home_ID"])
+	nb_homes = len(home_ids)
+	print("Number of Homes : ", nb_homes)
+	print("Number of Fluksos : ", len(sensors))
 
-    # =========================================================
+	# =========================================================
 
-    groups = getFLuksoGroups()
-    print("groups : ", groups)
-    homes = generateHomes(session, sensors, since, start_timing, to_timing, home_ids)
-    # grouped_homes = generateGroupedHomes(homes, groups)
+	groups = getFLuksoGroups()
+	print("groups : ", groups)
+	homes = generateHomes(session, sensors, since, start_timing, to_timing, home_ids)
+	# grouped_homes = generateGroupedHomes(homes, groups)
 
-    saveFluksoDataToCassandra(homes)
+	saveFluksoDataToCassandra(homes)
 
-    # 29-10-2021 from Midnight to midnight :
-    # --since s2021-10-29-00-00-00 --to s2021-10-30-00-00-00
-    # 17-12-2021 from Midnight to midnight :
-    # --since s2021-12-17-00-00-00 --to s2021-12-18-00-00-00
+	# 29-10-2021 from Midnight to midnight :
+	# --since s2021-10-29-00-00-00 --to s2021-10-30-00-00-00
+	# 17-12-2021 from Midnight to midnight :
+	# --since s2021-12-17-00-00-00 --to s2021-12-18-00-00-00
 
 
 if __name__ == "__main__":
-    main()
+	main()
