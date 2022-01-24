313070255a3877504a162f4023b4482c3e84f98e
AlexandreHnf <Alexandre.Heneffe@ulb.be>
2022-01-24 11:36:24 +0100

    radio buttons for user/admin + home_id/password

--- webapp/index.js
+++ webapp/index.js
@@ -1,5 +1,15 @@
 var login_button = document.getElementById("submit");
+var user_radio = document.getElementByName("user_radio");
+var admin_radio = document.getElementByName("admin_radio");
 
 function validateLoginForm() {
-	pass
+	var input_value = document.getElementById("userinput")  
+	if (input_value == "") {
+		text = "Veuillez fournir une entrée."
+	} else if (admin_radio.checked && input_value == "789"){
+		text = "OK admin";
+	} else if (user_radio.checked) {
+		text = "OK user";
+	}
+	document.getElementById("login_err_msg").innerHTML = text;
 }
